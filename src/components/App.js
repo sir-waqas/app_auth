2fb073a2bbabb7b8e6432060653e3a20073ce029
Waqas Azam <waqas.mabsapps@gmail.com>
2019-03-16 02:03:12 +0500

    Setting the style for root component (Custom One)

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 class App extends Component {
     render() {
         return (
-            <View>
+            <View style={{ flex: 1, marginTop: 50 }}>
                 <Text>An App</Text>
             </View >
         );
