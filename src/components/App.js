c64958593ab463904a63c7df2f4452723642f616
Waqas Azam <waqas.mabsapps@gmail.com>
2019-03-17 05:03:37 +0500

    Copying reuseable components

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
 class App extends Component {
     render() {
         return (
-            <View style={{ flex: 1, marginTop: 50 }}>
-                <Text>An App</Text>
+            // <View style={{ flex: 1, marginTop: 50 }}>
+            <View>
+                <Header headerText="Authentication" />
+                {/* <Text>An App</Text> */}
             </View >
         );
     }
 }
-
 export default App;
---
+++ src/components/common/index.js
@@ -0,0 +1,6 @@
+// This file will export all the components in one file 
+
+export * from './CustomButton';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
